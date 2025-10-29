70cf2e0563de6fb1c2a4c0bd1d4db79bdd94fd11
Twisuki <Twisuki@outlook.com>
2025-10-29 20:55:39 +0800

    :sparkles: 颜色与响应式初始工程化

---
+++ app/components/base/Footer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="footer-container">
+    <div class="footer">
+      Copyright &copy; {{ new Date().getFullYear() }} HMO - Apache 2.0 License
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.footer-container {
+  width: 100%;
+  height: 2rem;
+  background-color: var(--bg);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer {
+  width: var(--max-width);
+  padding: 0 var(--padding-x);
+  text-align: center;
+}
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <BaseNavbar />
+    <div class="content">
+      <slot />
+    </div>
+    <BaseFooter />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.content {
+  flex: 1;
+}
+</style>
