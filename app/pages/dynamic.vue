4ed72de5a11d6e733c91e22688e513ab4dd5831f
Twisuki <Twisuki@outlook.com>
2025-10-31 22:56:17 +0800

    :sparkles: 添加 content 支持

--- app/pages/dynamic.vue
+++ app/pages/dynamic.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-
+const { data: home } = await useAsyncData(() => queryCollection("content").path("/text").first())
 </script>
 
 <template>
-
+  <div class="page">
+    <ContentRenderer
+      v-if="home"
+      :value="home"
+      tag="article"
+    />
+    <div v-else>
+      Home not found
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/color-mode", "@nuxt/eslint", "@nuxt/eslint-config"],
+  modules: [
+    "@nuxtjs/color-mode",
+    "@nuxt/eslint",
+    "@nuxt/eslint-config",
+    "@nuxt/content",
+  ],
   ssr: true,
   devtools: { enabled: true },
   app: {
