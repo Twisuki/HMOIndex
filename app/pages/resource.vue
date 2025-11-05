c7cff7f342816326c96af997b86b4ad6668c8193
Twisuki <Twisuki@outlook.com>
2025-11-05 22:05:34 +0800

    :sparkles: 资源页面

--- app/pages/resource.vue
+++ app/pages/resource.vue
@@ -5,8 +5,57 @@ useHead({
 </script>
 
 <template>
+  <div class="page">
+    <BaseSection classname="title-container">
+      <template #title>
+        资源下载
+      </template>
+    </BaseSection>
+    <BaseSection classname="body-container">
+      <div class="content">
+        <span>
+          社团网盘搭建中
+        </span>
+        <span>
+          敬请期待
+        </span>
+      </div>
+    </BaseSection>
+  </div>
 </template>
 
 <style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0 var(--padding-x);
+}
 
+.title-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.body-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .content {
+    width: 100%;
+    height: 20rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    font-size: var(--title-size);
+    color: var(--text-light);
+  }
+}
 </style>
